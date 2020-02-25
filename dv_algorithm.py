babe5795a1426cf3af111541d31213688ec1dde0
JohnWolz <45606158+JohnWolz@users.noreply.github.com>
2020-02-25 14:05:42 -0600

    finished the program
    still need to pretty it up and format the output correctly, but the program works

--- dv_algorithm.py
+++ dv_algorithm.py
@@ -8,7 +8,27 @@ readCSV = csv.reader(open(file), delimiter=',')
 i = 0
 for row in readCSV:
     input_data.append(row)
-    input_data[i].remove(input_data[i][0]) # shaves off first column
+    input_data[i].remove(input_data[i][0]) # shaves off first column (labels)
     i+=1
+input_data.remove(input_data[0]) # removes first row (also labels)
 
 print(input_data)
+
+def DistanceVector(input_data):
+    for i in range (len(input_data)):
+        for j in range(len(input_data[1])):
+            if int(input_data[i][j]) != 0: # do not need to check distance between node and itself
+
+                # finds distance from start node to intermediate node and from intermediate node to end node
+                intermediate_index = (j+1) % len(input_data[i])
+                distance1 = int(input_data[i][intermediate_index])
+                distance2 = int(input_data[intermediate_index][j])
+
+                # compares distances
+                if (distance1 + distance2 < int(input_data[i][j])):
+                    input_data[i][j] = distance1 + distance2
+
+    return input_data
+
+new_distance_vectors = DistanceVector(input_data)
+print(new_distance_vectors)
